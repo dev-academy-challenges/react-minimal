82d6d1c914e3984fa691951111dfa76f836a1f9f
Don Smith <don@antiantonym.com>
2019-01-26 14:56:56 +1300

    move variable declaration

--- client/index.js
+++ client/index.js
@@ -1,13 +1,14 @@
 const React = require('react')
 const ReactDOM = require('react-dom')
 
+const data = {name: 'humans'}
+
 function helloTemplate (props) {
   return (
     <div>hello {props.name}</div>
   )
 }
 
-const data = {name: 'humans'}
 const view = helloTemplate(data)
 
 const placeToMount = document.getElementById('root')
